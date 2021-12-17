aa1db00cf132da5118e13f8bbbb55bfaddb13961
Nick Arabadzhi <91465047+Nickolas-26@users.noreply.github.com>
2021-12-17 16:54:47 +0200

    finish-1st-task

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,22 @@
 const navEl = document.querySelector('#categories');
 
-const items = navEl.querySelectorAll('item');
+const items = navEl.querySelectorAll('.item');
+
+console.log(`Number of categories: ${items.length}`);
+
+[...items].forEach(item => {
+    console.log(item)
+    console.log(item.querySelector('h2').textContent)
+    console.log(item.querySelectorAll('li').length)
+
+})
+
+
+// -----
+ 
+// const itemList = [...items].map(item => {
+//     return `Category :  ${item.querySelector('h2').textContent}\n
+//     elements: ${item.querySelectorAll('li').length}`;
+
+// }).join('\n')
+// console.log(itemList)
