58efdd745ee00d3ddf923cec07b95ec466eb9dce
Nick Arabadzhi <91465047+Nickolas-26@users.noreply.github.com>
2021-12-21 16:24:47 +0200

    update-task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,20 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
-
-
-const markup = ingredients.map(item => {
-  const li = document.createElement('li')
-
-  console.log(li);
-  li.classList.add('item')
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+const refs = {
+  ul: document.querySelector("#ingredients"),
+};
+const markup = ingredients.map((item) => {
+  const li = document.createElement("li");
+  // console.log(li);
   li.textContent = item;
+  li.classList.add("item");
+
   return li;
-
-})
-
+});
+refs.ul.append(...markup);
